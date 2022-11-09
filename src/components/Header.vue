56a40df5d23b5d9abd5ef14d161ab0725ea5a21e
soya <3100034521@qq.com>
2022-11-09 09:11:26 +0800

    fix: modify the title

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,10 @@
-<script lang="ts" setup>
-import { useSearchStore } from '@/store'
-
-const { packSource } = useSearchStore()
-</script>
-
 <template>
   <Div
     width="100"
     justifyContent="center"
   >
     <Span color="yellow">
-      VTUI NPM SEARCH FROM {{ packSource }}
+      VTUI NPM
     </Span>
   </Div>
 </template>
